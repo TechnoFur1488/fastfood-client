3788d1cd734fde22bdd8297a0e2614abe9ea005e
Nikita <nikitaloginovxxx1986@gmail.com>
2025-07-01 01:23:46 +0300

    update

--- app/catalog/[catalogId]/page.tsx
+++ app/catalog/[catalogId]/page.tsx
@@ -1,27 +1,23 @@
 import { CatalogPage } from "@/components/shared/catalog-page";
 import { Container } from "@/components/shared/container";
-import { cookies } from "next/headers";
-import jwt, { JwtPayload } from "jsonwebtoken"
+import { useGetRoleQuery } from "@/store/apiSlice";
 
 export default async function PageCatalog() {
-    const jwtCookie = await cookies()
-    const token = jwtCookie.get('jwt')?.value
+    const { data } = useGetRoleQuery()
 
-    let role = null
-    if (token) {
-        try {
-            const decoded = jwt.verify(token, process.env.JWT_SECRET || "")
-            if (typeof decoded === "object" && decoded !== null && "role" in decoded) {
-                role = (decoded as JwtPayload).role as string;
-            }
-        } catch {
-            role = null
-        }
+    if (data?.role !== "ADMIN") {
+        return (
+            // notFound()
+            <div>
+                не админ
+                {"роль:" + data?.role}
+            </div>
+        );
     }
 
     return (
         <Container>
-            <CatalogPage role={role} />
+            <CatalogPage role={data.role} />
         </Container>
     )
 }
