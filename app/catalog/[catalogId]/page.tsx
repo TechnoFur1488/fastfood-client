8775ab2e41d90d43533a40006ba286e0775efdaa
Nikita <nikitaloginovxxx1986@gmail.com>
2025-07-01 03:21:22 +0300

    update

--- app/catalog/[catalogId]/page.tsx
+++ app/catalog/[catalogId]/page.tsx
@@ -7,19 +7,9 @@ import { useGetRoleQuery } from "@/store/apiSlice";
 export default function PageCatalog() {
     const { data } = useGetRoleQuery()
 
-    if (data?.role !== "ADMIN") {
-        return (
-            // notFound()
-            <div>
-                не админ
-                {"роль:" + data?.role}
-            </div>
-        );
-    }
-
     return (
         <Container>
-            <CatalogPage role={data.role} />
+            <CatalogPage role={data?.role} />
         </Container>
     )
 }
